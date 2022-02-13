9df254a8547b6abaeb98c239d2dd843368c1c12e
NishatFariza <nishatfarzia78@gmail.com>
2022-02-14 01:48:01 +0600

    phone balance added

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+function updateCaseNumber(isIncreasing){
+    const caseInput =document.getElementById('case-number');
+   let caseInputNumber =caseInput.value;
+
+   if(isIncreasing == true){
+    caseInputNumber =parseInt(caseInputNumber) + 1 ;
+   }
+  else if (caseInputNumber > 0){
+    caseInputNumber =parseInt(caseInputNumber) - 1 ;
+  }
+  caseInput.value =caseInputNumber;
+
+  //update case total
+  const caseTotal = document.getElementById('case-total');
+  caseTotal.innerText = caseInputNumber * 59;
+}
+
+
 document.getElementById('case-plus').addEventListener('click', function(){
-    console.log('this is plus button');
+   updateCaseNumber(true);
+//    console.log(caseInputNumber);
+});
+document.getElementById('case-minus').addEventListener('click', function(){
+    updateCaseNumber(false);
 })
